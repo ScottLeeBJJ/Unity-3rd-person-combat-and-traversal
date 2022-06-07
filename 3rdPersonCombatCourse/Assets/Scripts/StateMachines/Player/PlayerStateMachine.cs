5876b354bf8e1e39ca3f42bd8b88fb2aec303698
ScottLeeBJJ <scottlee5455@outlook.com>
2022-06-07 10:02:28 -0700

    working on player character controller

--- 3rdPersonCombatCourse/Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ 3rdPersonCombatCourse/Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -6,6 +6,10 @@ public class PlayerStateMachine : StateMachine
 {
     [field: SerializeField] public InputReader InputReader { get; private set; }
 
+    [field: SerializeField] public CharacterController Controller { get; private set; }
+    [field: SerializeField] public float FreeLookMovementSpeed { get; private set; }
+
+
     private void Start()
     {
         SwitchState(new PlayerTestState(this));
